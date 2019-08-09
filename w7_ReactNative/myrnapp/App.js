30acbc21f259c12916a0267c751f3433e68f33d3
laoxie <jeroo@qq.com>
2019-08-09 09:15:18 +0800

    ReactNative课件哟话

--- w7_ReactNative/myrnapp/App.js
+++ w7_ReactNative/myrnapp/App.js
@@ -20,9 +20,10 @@ type Props = {};
 export default class App extends Component<Props> {
 
   render() {
+    console.log(666)
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.welcome}>Hello, laoxie, Are you ok! Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
